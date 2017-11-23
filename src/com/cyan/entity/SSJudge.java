39a6439a6798dcc1d526b9b4bf0660bc2564b9d7
yinghuacheng <714026326@qq.com>
2017-11-23 19:22:16 +0800

    hello,everybody,i am the judge entry

---
+++ src/com/cyan/entity/SSJudge.java
@@ -0,0 +1,67 @@
+package com.cyan.entity;
+
+public class SSJudge {
+    private String id;
+    private String phone;
+    private String age;
+    private String identity;
+    private String pwd;
+    private String name;
+    private String teamId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+}
