2663f9dec240c6732c609b65a408e26ef9eb0b46
yinghuacheng <714026326@qq.com>
2017-11-23 19:21:48 +0800

    hello,everybody,i am the athlete entry

---
+++ src/com/cyan/entity/SSAthlete.java
@@ -0,0 +1,76 @@
+package com.cyan.entity;
+
+public class SSAthlete {
+    private String id;
+    private String name;
+    private String identity;
+    private String age;
+    private String sex;
+    private String tel;
+    private String score;
+    private String teamId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+}
