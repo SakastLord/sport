2a2e21f29529407c93803e329fed167bfee285c8
yinghuacheng <714026326@qq.com>
2017-11-23 19:23:00 +0800

    hello,everybody

---
+++ src/com/cyan/entity/SSTeam.java
@@ -0,0 +1,103 @@
+package com.cyan.entity;
+
+public class SSTeam {
+    private String id;
+    private String teamName;
+    private String leaderName;
+    private String leaderIdentity;
+    private String leaderPhone;
+    private String doctorName;
+    private String doctorPhone;
+    private String coachName;
+    private String coachPhone;
+    private String coachId;
+    private String pwd;
+
+    public String getCoachId() {
+        return coachId;
+    }
+
+    public String getCoachName() {
+        return coachName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+
+    public void setLeaderName(String leaderName) {
+        this.leaderName = leaderName;
+    }
+
+    public String getLeaderIdentity() {
+        return leaderIdentity;
+    }
+
+    public void setLeaderIdentity(String leaderIdentity) {
+        this.leaderIdentity = leaderIdentity;
+    }
+
+    public String getLeaderPhone() {
+        return leaderPhone;
+    }
+
+    public void setLeaderPhone(String leaderPhone) {
+        this.leaderPhone = leaderPhone;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getDoctorPhone() {
+        return doctorPhone;
+    }
+
+    public void setDoctorPhone(String doctorPhone) {
+        this.doctorPhone = doctorPhone;
+    }
+
+    public void setCoachName(String coachName) {
+        this.coachName = coachName;
+    }
+
+    public String getCoachPhone() {
+        return coachPhone;
+    }
+
+    public void setCoachPhone(String coachPhone) {
+        this.coachPhone = coachPhone;
+    }
+
+    public void setCoachId(String coachId) {
+        this.coachId = coachId;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
